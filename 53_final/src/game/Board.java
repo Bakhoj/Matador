e055bc8eda859654d59efd6fe577b0b691cecf5d
Bakhoj <Anders2108@hotmail.com>
2014-01-12 17:40:25 +0100

    Test.

--- 53_final/src/game/Board.java
+++ 53_final/src/game/Board.java
@@ -5,6 +5,10 @@ import boundaryToMatador.GUI;
 public class Board {
 	private Field[] boardArray = new Field[40];
 	
+	// Nå... det virker sådan her. Jeg laver denne ændring nu, ved at lave en kommentering. Den uploader vi sådan her:
+	// Først gem.
+	// Der kommer en pil ved navnet. Betyder at der er lavet en ændring.
+	
 	public Board() {
 		boardArray[0] = new Start("Start"); // Fixet
 		boardArray[1] = new Street("RÃ¸dovrevej", 1200, 1, int rent0, int rent1, int rent2, int rent3, int rent4, int rent5);
